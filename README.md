384e15b49cddc550e048424fee933477aaf8f81e
Andre Cantarino <cantarino.andre@gmail.com>
2016-04-16 21:25:08 -0300

    versao final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # axado-python
+Executavel python para calculo de frete da Axado
+
+### COMO FUNCIONA
+1. Você deverá baixar o projeto do github: git@github.com:andrecantarino/axado-python.git
+2. As regras dos calculos estao definidas no arquivo 'readme.txt'
+3. Depois de baixado o projeto acesse o terminal e execute o comando 'python axado.py florianopolis brasilia 50 7'
+4. O resultado do calculo do frete sera exibido em duas linhas abaixo de acordo com as tabelas 1 e tabela 2
--- axado.py
+++ axado.py
@@ -11,121 +11,124 @@ class Axado(object):
 	def multiplica_por_dois(self):
 		return self.x * 2
 
+	def calculoFreteTabela1(self, origem, destino, nota_fiscal, peso):
+		valor_seguro = 0.0
+		fixa = 0.0
+		faixa = 0.0
+		valor_tabela1 = 0.0
+		icms = 6 #o icms sempre sera 6% para a tabela 1
+		prazo = "0"
+
+		#percorrer a tabela de rotas e encontrar os parametro conforme origem e destino
+		f = csv.reader(open('tabela/rotas.csv'), delimiter=',')
+		#os dados do arquivo serao separados nas variaveis referente sua informacao
+		for [rt_origem, rt_destino, rt_prazo, rt_seguro, rt_kg, rt_fixa] in f:
+			#se origem e destino passados por parametro encontrados no arquivo
+			if origem == rt_origem and destino == rt_destino:
+				#calcular seguro: <(valor da nota fiscal * taxa do seguro)/100>
+				valor_seguro = float(nota_fiscal)*float(rt_seguro)/100
+				#valor da taxa fixa
+				fixa = float(rt_fixa)
+				#prazo 
+				prazo = rt_prazo
+
+				#ler arquivo com as faixas de valores cobrados por peso de acordo com a faixa de peso
+				p = csv.reader(open('tabela/preco_por_kg.csv'), delimiter=',')
+				#os dados do arquivo serao separados nas variaveis referente sua informacao
+				for [p_nome, p_inicial, p_final, p_preco] in p:
+					#compara o nome das faixas referencia para cobranca de frente por kilograma
+					if rt_kg.strip() == p_nome.strip():
+						#verificar se o peso final foi declarado na tabela de cobranca
+						if len(p_final.strip()) > 0:
+							if rt_kg.strip() == p_nome.strip() and (float(peso) >= float(p_inicial) and float(peso) < float(p_final)):							
+								faixa = float(peso) * float(p_preco)
+								break
+						else:
+							faixa = float(peso) * float(p_preco)
+							break
+
+				#calculo do valor do frete da tabela 1 (ja foi calculado a divisao do icms)
+				valor_tabela1 = (float(valor_seguro) + float(fixa) + float(faixa)) / 0.94				
+
+		return 'tabela: %s, %.2f' % (prazo, valor_tabela1)
+
+	def calculoFreteTabela2(self, origem, destino, nota_fiscal, peso):
+		valor_seguro = 0.0
+		fixa = 0.0
+		faixa = 0.0
+		icms = 0.0
+		alfandega = 0.0
+		valor_tabela2 = 0.0
+		prazo = "0"
+		status = 1
+
+		#coverter o arquivo .tsv em .csv e realizar a leituras das rotas
+		with open('tabela2/rotas.tsv','rb') as tsvin_rt, open('tabela2/rotas.csv', 'wb') as csvout_rt:
+			tsvin_rt = csv.reader(tsvin_rt, delimiter='\t')
+			csvout_rt = csv.writer(csvout_rt)
+
+			#percorrer as linhas com todas as rotas
+			for r_rotas in tsvin_rt:
+				#se encontrar origem e destino compativel com o parametro
+				if origem == r_rotas[0] and destino == r_rotas[1]:
+					#calcular seguro: <(valor da nota fiscal * taxa do seguro)/100>
+					valor_seguro = float(nota_fiscal)*float(r_rotas[4])/100
+					#prazo
+					prazo = r_rotas[3]
+
+					#coverter o arquivo .tsv em .csv e realizar a leituras das faixas de preco por peso
+					with open('tabela2/preco_por_kg.tsv','rb') as tsvin_pk, open('tabela2/preco_por_kg.csv', 'wb') as csvout_pk:
+						tsvin_pk = csv.reader(tsvin_pk, delimiter='\t')
+						csvout_pk = csv.writer(csvout_pk)
+
+						#percorrer as linhas do arquivo
+						for r_preco in tsvin_pk:
+							#comparar siglas de cobranca por faixa de preco
+							if r_preco[0].strip() == r_rotas[7].strip(): 																					
+								#se limite de cobranca for 0
+								if r_preco[0].strip() == r_rotas[7].strip(): 																					
+									if float(r_rotas[2]) == 0:
+										faixa = float(peso) * float(r_preco[3])
+										break
+								
+									if len(r_preco[2]) > 0:
+										if float(r_preco[2]) < float(r_rotas[2]):
+											faixa = float(peso) * float(r_preco[3])
+									else:
+										status = 0
+										break
+
+					#calculo alfandega: <subtotal * (taxa alfandega/100)>
+					alfandega = (float(valor_seguro) + float(fixa) + float(faixa)) * (float(r_rotas[6]) / float(100))
+					#icms de acordo com a origem e o destino
+					icms = float(r_rotas[5])
+					#valor do frete <subtotal (com a taxa de alfandega) / calculo icms>
+					if status == 1:
+						valor_tabela2 = (float(valor_seguro) + float(fixa) + float(faixa) + float(alfandega)) / ((100 - float(icms))/100)
+					else:
+						prazo = '-'
+						valor_tabela2 = '-'
+					
+			if valor_tabela2 == '-':
+				return 'tabela 2: %s, %s' % (prazo, valor_tabela2)
+			else:
+				return 'tabela 2: %s, %.2f' % (prazo, valor_tabela2)
+
 if __name__ == "__main__":
-	# ax = Axado(x=10)
-	# print ax.x
-	# print "%d vezes 2 e %d\n" % (ax.x, ax.multiplica_por_dois())
+	#instancia da classe Axado
+	ax = Axado(x=10)
 	
+	#ler os parametros para calculo do frete
 	param = sys.argv[1:]
 	if len(param) == 4:
-		#lendo os parametros
-		print(param)
 		origem = param[0]
 		destino = param[1]
 		nota_fiscal = param[2]
 		peso = param[3]
-		print origem
-		print destino
-		print nota_fiscal
-		print peso
 		
-		#lendo o arquivo
-		#print __doc__
-		f = csv.reader(open('tabela/rotas.csv'), delimiter=',')
-		for [rt_origem, rt_destino, rt_prazo, rt_seguro, rt_kg, rt_fixa] in f:
-			# print 'origem=%s | destino=%s | prazo=%s | seguro=%s | kg=%s | fixa: %s' % (origem, destino, prazo, seguro, kg, fixa)
-			# print f.line_num, 'linhas lidas'
-			# print '--- fim'
-			if origem == rt_origem and destino == rt_destino:
-				#print 'origem=%s | destino=%s | prazo=%s | seguro=%s | kg=%s | fixa: %s' % (rt_origem, rt_destino, rt_prazo, rt_seguro, rt_kg, rt_fixa)
-				valor_seguro = float(nota_fiscal)*float(rt_seguro)/100
-				fixa = float(rt_fixa)
-				faixa = 0
-
-				#ler arquivo com as faixas de valores por peso
-				p = csv.reader(open('tabela/preco_por_kg.csv'), delimiter=',')
-				for [p_nome, p_inicial, p_final, p_preco] in p:
-					#print 'p_nome=%s | p_inicial=%s | p_final=%s | p_preco=%s' % (p_nome, p_inicial, p_final, p_preco)
-					#print '%s - %s - %s - %s' % (rt_kg, p_inicial, p_final, peso)
-					
-					# if (p_final):
-					# 	if rt_kg.strip() == p_nome.strip() and (float(peso) >= float(p_inicial) and float(peso) < float(p_final)):							
-					# 		faixa = float(peso) * float(p_preco)	
-
-					if rt_kg.strip() == p_nome.strip():
-						if (p_final):
-							if (float(peso) >= float(p_inicial) and float(peso) < float(p_final)):							
-								faixa = float(peso) * float(p_preco)
-						else:
-							faixa = float(peso) * float(p_preco)
-
-				#quo = 94 / 100
-				#print 'quo: %s' % quo
-				icms = 6
-				valor_tabela1 = (float(valor_seguro) + float(fixa) + float(faixa)) / 0.94				
-				print 'seguro: <nota_fiscal> %s * <seguro> %s / 100 = %.2f' % (nota_fiscal, rt_seguro, valor_seguro)
-				print 'fixa: <proprio valor da taxa> = %2.f' % (fixa)
-				print 'faixa: <valor de acordo com o peso> = %2.f' % (faixa)
-				print 'valor_seguro + fixa + faixa <subtotal antes do icms> = %s' % (valor_seguro+fixa+faixa)
-				print 'icms: na <tabela 1> sempre sera 6 = %s' % (icms)
-				print ''
-				print '========'
-				print ''
-				print 'tabela: %s, %s' % (rt_prazo, valor_tabela1)
-
-		print ''
-		print '========'
-		print ''
-		
-		valor_seguro = 0
-		fixa = 0
-		faixa = 0
-		icms = 0
-		alfandega = 0
-		valor_tabela2 = 0
-		with open('tabela2/rotas.tsv','rb') as tsvin_rt, open('rotas.csv', 'wb') as csvout_rt:
-			tsvin_rt = csv.reader(tsvin_rt, delimiter='\t')
-			csvout_rt = csv.writer(csvout_rt)
-
-			for r_rotas in tsvin_rt:
-				#print '%s' % r_rotas
-				if origem == r_rotas[0] and destino == r_rotas[1]:
-					valor_seguro = float(nota_fiscal)*float(r_rotas[4])/100
-					
-					with open('tabela2/preco_por_kg.tsv','rb') as tsvin_pk, open('preco_por_kg.csv', 'wb') as csvout_pk:
-						tsvin_pk = csv.reader(tsvin_pk, delimiter='\t')
-						csvout_pk = csv.writer(csvout_pk)
-
-						for r_preco in tsvin_pk:
-							if r_preco[0].strip() == r_rotas[7].strip(): 																					
-								if float(r_rotas[2]) == 0:
-									faixa = float(peso) * float(r_preco[3])
-									break
-								
-								if float(r_preco[2]) < float(r_rotas[2]):
-									faixa = float(peso) * float(r_preco[3])
-
-								# if r_rotas[2]:
-								# 	if float(r_preco[2]) < float(r_rotas[2]):
-								# 		faixa = float(peso) * float(r_preco[3])
-								# else:
-								# 	faixa = float(peso) * float(r_preco[3])
-
-					alfandega = (float(valor_seguro) + float(fixa) + float(faixa)) * (float(r_rotas[6]) / float(100))
-					icms = float(r_rotas[5])
-					valor_tabela2 = (float(valor_seguro) + float(fixa) + float(faixa) + float(alfandega)) / ((100 - float(icms))/100)
-					print ''
-					print 'seguro: <nota_fiscal> %s * <seguro> %s / 100 = %.2f' % (nota_fiscal, r_rotas[4], valor_seguro)
-					print 'fixa: <proprio valor da taxa> = %s' % (fixa)
-					print 'faixa: <valor de acordo com o peso> = %s' % (faixa)
-					print '<subtotal> = %s' % (valor_seguro + fixa + faixa)
-					print '<alfandega> = %s' % (r_rotas[6])
-					print 'calculo alfandega: <subtotal * (alfandega/100)> = %s' % (alfandega)
-					print '<subtotal antes do icms> = %s' % (valor_seguro + fixa + faixa + alfandega)
-					print 'icms: na <tabela 2> = %s' % (icms)
-					print ''
-					print '========'
-					print ''
-					print 'tabela 2: %s, %s' % (r_rotas[3], valor_tabela2)
+		#calcular o valor do frete da tabela 1
+		print ax.calculoFreteTabela1(origem, destino, nota_fiscal, peso)
+		#calcular o valor do frete da tabela 2
+		print ax.calculoFreteTabela2(origem, destino, nota_fiscal, peso)
 	else:
 		print "Por favor informar todos os parametros para calcular o frete"
